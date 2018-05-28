eb89bbd35035a5ee550adf03452a392faa84ea6a
pandaman <aeonzamaek@gmail.com>
2018-05-28 21:56:19 +0100

    More game logic

--- logic.lisp
+++ logic.lisp
@@ -1,49 +1,47 @@
 ; turn: p1 or p2
 (defparameter *turn* 'p1)
-
-
 ; score: integer from 0 to 3
 (defparameter *score* '(0 0))
-
-
-; state: ready, ask, reply
+; state: ready, ask, reply, done
 (defparameter *state* 'ready)
-
-
 ; success_counter: integer to swap turn when it reaches 3
-(defparameter *success_counter* 0)
+(defparameter *success-counter* 0)
 
 
 (defun start ()
   (print '(let the game begin)))
 
 
-(defun turnGlobal (success)
-  (and (updateTurn success)
-       (updateState success)))
+(defun turn (success)
+  (and (update-turn success)
+       (update-state success)
+       (update-score success)))
 
 
-(defun updateTurn (success)
+(defun update-turn (success)
   (cond ((eq *turn* 'p1) (setf *turn* 'p2))
 	((eq *turn* 'p2) (setf *turn* 'p1))
 	(t (print '(unknown state during turn)))))
 
 
-(defun updateState (success)
+(defun update-state (success)
   (cond ((equal *state* 'ready) (setf *state* 'ask))
 	((equal *state* 'ask )  (setf *state* 'reply))
 	((equal *state* 'reply) (setf *state* 'ask))))
 
 
+(defun update-score ()
+  ())
+
+
 ; Should we check for MAX_SCORE here?
-(defun updateSuccessCounter (success)
+(defun update-success-ounter (success)
   (if (success)
-      (setf *success_counter* (1+ *success_counter*))))
+      (setf *success-counter* (1+ *success-counter*))))
 
 
-; Omitting the else is valid in CL
-(defparameter *test-counter* 0)
-(defun testInc (test-cond)
-  (if '(test-cond)
-      (setf *test-counter* (1+ *test-counter*))))
-
+(defun print-state ()
+  (and (print *turn*)
+       (print *score*)
+       (print *state*)
+       (print *success_counter*)))
