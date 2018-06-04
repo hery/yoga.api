aeb96d18f3c18687fbe4863193dbf23054802d25
pandaman <aeonzamaek@gmail.com>
2018-06-04 22:43:07 +0100

    Better logic API

--- logic.lisp
+++ logic.lisp
@@ -1,15 +1,48 @@
 ; turn: p1 or p2
 (defparameter *turn* 'p1)
+
 ; score: integer from 0 to 3
 (defparameter *score* '(0 0))
+
 ; state: ready, ask, reply, done
 (defparameter *state* 'ready)
+
 ; success_counter: integer to swap turn when it reaches 3
 (defparameter *success-counter* 0)
 
+; tentative alternative API
+; (turn 'ask image)
+; (turn 'reply image)
+; (turn 'validate 't) or (turn 'validate nil)
+; signature: (turn '(action payload) ())
+
+
+(defun turn-with-params (action &optional payload)
+  (cond ((eq action 'ask)      (handle-ask payload))
+	((eq action 'reply)    (handle-reply payload))
+	((eq action 'validate) (handle-validate payload))))
+
+
+(defun handle-ask (image)
+  (print `(handling ASK with image ,image)))
+
+
+
+(defun handle-reply (image)
+  (print `(handling REPLY with image ,image)))
+
+
+(defun handle-validate (success)
+  (print `(handling VALIDATE with success ,success)))
+
+
+; Question: Can we structure our API like a REPL?
+; e.g. Payload = turn validate t token
+;                turn reply image token
 
 (defun start ()
-  (setf *state* 'ask))
+  (progn (setf *turn* 'p1)
+	 (setf *state* 'ask)))
 
 
 (defun turn ()
@@ -24,21 +57,16 @@
 	(t (print '(unknown state during turn)))))
 
 
+(defun swap-turns ()
+  (update-turn))
+
+
 (defun update-state ()
   (cond ((equal *state* 'ready) (setf *state* 'ask))
 	((equal *state* 'ask )  (setf *state* 'reply))
 	((equal *state* 'validate) (setf *state* 'ask))
 	((equal *state* 'reply) (setf *state* 'validate))))
 
-
-; tentative alternative API
-; (turn 'ask)
-; (turn 'reply image)
-; (turn 'validate 't) or (turn 'validate nil)
-
-; Question: Can we structure our API like a REPL?
-; e.g. Payload = turn validate t token
-;                turn reply image token
 
 (defun update-score ()
   ())
